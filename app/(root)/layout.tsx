428c2d686045d1a55698de33cf466d4cca297b9f
golfballnut <linkschoicesteve@gmail.com>
2024-12-17 17:39:18 -0500

    Integrate GitHub provider into NextAuth and remove default home page component

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="font-work-sans">
+      
+      
+      
+      {children}
+    </main>
+  )
+}
